fd7dc27c54a475a8643fc37d1e9a757de1c69b82
gaotianpeng <gtpsydm@gmail.com>
2018-04-09 21:32:44 +0800

    hello tdd

--- source/test/test_main.cpp
+++ source/test/test_main.cpp
@@ -1,72 +1,11 @@
-#include <limits.h>
-#include "sample.h"
-#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 #include "easylogging++.h"
-
-#include "../me/math.h"
-#include "rapidjson/document.h"
-#include "rapidjson/prettywriter.h"
-#include "../utility/sys_info.h"
-#include "Psapi.h"
+#include <limits.h>
 
 INITIALIZE_EASYLOGGINGPP
 
-using ::testing::InitGoogleTest;
-
-namespace {
-
-TEST(FactorialTest, Negative) {
-
-  EXPECT_EQ(1, Factorial(-5));
-  EXPECT_EQ(1, Factorial(-1));
-  EXPECT_GT(Factorial(-10), 0);
-}
-
-// Tests factorial of 0.
-TEST(FactorialTest, Zero) {
-  EXPECT_EQ(1, Factorial(0));
-}
-
-// Tests factorial of positive numbers.
-TEST(FactorialTest, Positive) {
-  EXPECT_EQ(1, Factorial(1));
-  EXPECT_EQ(2, Factorial(2));
-  EXPECT_EQ(6, Factorial(3));
-  EXPECT_EQ(40320, Factorial(8));
-}
-
-TEST(IsPrimeTest, Negative) {
-  // This test belongs to the IsPrimeTest test case.
-
-  EXPECT_FALSE(IsPrime(-1));
-  EXPECT_FALSE(IsPrime(-2));
-  EXPECT_FALSE(IsPrime(INT_MIN));
-}
-
-// Tests some trivial cases.
-TEST(IsPrimeTest, Trivial) {
-  EXPECT_FALSE(IsPrime(0));
-  EXPECT_FALSE(IsPrime(1));
-  EXPECT_TRUE(IsPrime(2));
-  EXPECT_TRUE(IsPrime(3));
-}
-
-// Tests positive input.
-TEST(IsPrimeTest, Positive) {
-  EXPECT_FALSE(IsPrime(4));
-  EXPECT_TRUE(IsPrime(5));
-  EXPECT_FALSE(IsPrime(6));
-  EXPECT_TRUE(IsPrime(23));
-}
-}  // namespace
-
-
 int main(int argc, char **argv) 
 {
-	InitGoogleTest(&argc, argv);
-
-
-	int ret_val = RUN_ALL_TESTS();
-	system("pause"); 
-	return ret_val;
+	testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS(); 
 }
